3c808b868f395f923eb7200b8f74cd050cb3a8f9
Charly Bernard <charly.bernard@gmail.com>
2022-12-21 16:15:25 +0100

    build: deployment to mainnet

--- src/config.js
+++ src/config.js
@@ -1,13 +1,15 @@
 import { ethers } from 'ethers';
 
+const nodeUrl = `${import.meta.env.VITE_APP_INFURA_URL}/${
+  import.meta.env.VITE_APP_INFURA_ID
+}`;
+
 export default {
   etherscanUrl: import.meta.env.VITE_APP_ETHERSCAN_URL,
   targetChainId: import.meta.env.VITE_APP_TARGET_CHAIN_ID,
   targetChainName: import.meta.env.VITE_APP_TARGET_CHAIN_NAME,
   targetChainShortName: import.meta.env.VITE_APP_TARGET_CHAIN_SHORT,
-  nodeUrl: import.meta.env.VITE_APP_NODE_URL,
+  nodeUrl: nodeUrl,
   infuraId: import.meta.env.VITE_APP_INFURA_ID,
-  provider: new ethers.providers.JsonRpcProvider(
-    import.meta.env.VITE_APP_NODE_URL
-  ),
+  provider: new ethers.providers.JsonRpcProvider(nodeUrl),
 };
--- src/utils/infura.js
+++ src/utils/infura.js
@@ -1,10 +1,7 @@
 const topics = {
-  Position:
-    '0x591ede549d7e337ac63249acd2d7849532b0a686377bbf0b0cca6c8abd9552f2',
-  ChallengeStarted:
-    '0xc4b384b2c5ca32c8e77081f4083be594a1ea9ba34f208a9f9a458f70608585f5',
-  ChallengeSucceeded:
-    '0x05f9cc3345665aee5729d9564c27530d8b2ad3b4eb7e5d2503b15dc98498e726',
+  Position: import.meta.env.VITE_APP_TOPIC_POSITION,
+  ChallengeStarted: import.meta.env.VITE_APP_CHALLENGE_STARTED,
+  ChallengeSucceeded: import.meta.env.VITE_APP_CHALLENGE_SUCCEEDED,
 };
 
 export default async (topic) => {
@@ -24,7 +21,9 @@ export default async (topic) => {
   };
 
   const response = await fetch(
-    'https://sepolia.infura.io/v3/692b1e384ae347e8b8e795d6df4fbc08',
+    `${import.meta.env.VITE_APP_INFURA_URL}/${
+      import.meta.env.VITE_APP_INFURA_ID
+    }`,
     {
       method: 'POST',
       body: JSON.stringify(params),
